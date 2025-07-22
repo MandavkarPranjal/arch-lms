edd172059a5d2a2734816591c71fade31a9e1264
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-07-22 14:44:17 +0530

    fix public navbar

--- app/(public)/_components/navbar.tsx
+++ app/(public)/_components/navbar.tsx
@@ -118,7 +118,7 @@ export function Navbar() {
 
                  <div className="flex items-center space-x-4">
                  */}
-                <div>
+                <div className="flex items-center space-x-4">
 
                     <ThemeToggle />
 
