900dc3993314f67eebf58649a0d6333e0250feca
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-07-26 23:04:30 +0530

    added server-only code

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,28 +1,30 @@
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "./db";
-import { env } from "./env";
-import { emailOTP } from "better-auth/plugins";
-import { resend } from "./resend";
-import OtpEmail from "@/components/email/otp";
+import 'server-only';
+
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { emailOTP } from 'better-auth/plugins';
+import OtpEmail from '@/components/email/otp';
+import { betterAuth } from 'better-auth';
+import { resend } from './resend';
+import { prisma } from './db';
+import { env } from './env';
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+        provider: 'postgresql', // or "mysql", "postgresql", ...etc
     }),
     socialProviders: {
         google: {
             clientId: env.GOOGLE_CLIENT_ID,
             clientSecret: env.GOOGLE_CLIENT_SECRET,
-            accessType: "offline",
-            prompt: "select_account",
+            accessType: 'offline',
+            prompt: 'select_account',
         },
     },
     plugins: [
         emailOTP({
             async sendVerificationOTP({ email, otp }) {
                 await resend.emails.send({
-                    from: 'Acme <onboarding@resend.dev>',
+                    from: 'Arch-LMS <onboarding@resend.dev>',
                     to: [email],
                     subject: 'Arch LMS - Verify your email',
                     react: OtpEmail({ otp }),
