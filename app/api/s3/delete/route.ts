580540c2f809fa8d84df77663e3e268044672efd
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-07-29 09:10:38 +0530

    feat: bot detection and rate limiting on file upload and delete

--- app/api/s3/delete/route.ts
+++ app/api/s3/delete/route.ts
@@ -1,10 +1,38 @@
+import arcjet, { detectBot, fixedWindow } from '@/lib/arcjet';
 import { DeleteObjectCommand } from '@aws-sdk/client-s3';
 import { NextResponse } from 'next/server';
+import { headers } from 'next/headers';
 import { S3 } from '@/lib/S3Client';
+import { auth } from '@/lib/auth';
 import { env } from '@/lib/env';
 
+const aj = arcjet
+    .withRule(
+        detectBot({
+            mode: 'LIVE',
+            allow: [],
+        }),
+    )
+    .withRule(
+        fixedWindow({
+            mode: 'LIVE',
+            window: '1m',
+            max: 5,
+        }),
+    );
 export async function DELETE(request: Request) {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
     try {
+        const decision = await aj.protect(request, {
+            fingerprint: session?.user.id as string,
+        });
+
+        if (decision.isDenied()) {
+            return NextResponse.json({ error: 'Access denied' }, { status: 429 });
+        }
+
         const body = await request.json();
 
         const key = body.key;
--- app/api/s3/upload/route.ts
+++ app/api/s3/upload/route.ts
@@ -1,10 +1,28 @@
+import arcjet, { detectBot, fixedWindow } from '@/lib/arcjet';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { PutObjectCommand } from '@aws-sdk/client-s3';
 import { NextResponse } from 'next/server';
+import { headers } from 'next/headers';
 import { S3 } from '@/lib/S3Client';
 import { v4 as uuidv4 } from 'uuid';
+import { auth } from '@/lib/auth';
 import { env } from '@/lib/env';
 import z from 'zod/v3';
+
+const aj = arcjet
+    .withRule(
+        detectBot({
+            mode: 'LIVE',
+            allow: [],
+        }),
+    )
+    .withRule(
+        fixedWindow({
+            mode: 'LIVE',
+            window: '1m',
+            max: 5,
+        }),
+    );
 
 export const fileUploadSchema = z.object({
     fileName: z.string().min(1, { message: 'File name is required' }),
@@ -14,7 +32,18 @@ export const fileUploadSchema = z.object({
 });
 
 export async function POST(request: Request) {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
     try {
+        const decision = await aj.protect(request, {
+            fingerprint: session?.user.id as string,
+        });
+
+        if (decision.isDenied()) {
+            return NextResponse.json({ error: 'Access denied' }, { status: 429 });
+        }
+
         const body = await request.json();
 
         const validation = fileUploadSchema.safeParse(body);
