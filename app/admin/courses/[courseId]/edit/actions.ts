3c80b3d10382fc6dca4d130ef447ff2b0ab35f68
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-07-30 19:15:26 +0530

    fix: edit action is protected by arcjet

--- app/admin/courses/[courseId]/edit/actions.ts
+++ app/admin/courses/[courseId]/edit/actions.ts
@@ -2,13 +2,49 @@
 
 import { courseSchema, CourseSchemaType } from '@/lib/zodSchemas';
 import { requireAdmin } from '@/app/data/admin/require-admin';
+import arcjet, { detectBot, fixedWindow } from '@/lib/arcjet';
 import { ApiResponse } from '@/lib/types';
+import { request } from '@arcjet/next';
 import { prisma } from '@/lib/db';
+
+const aj = arcjet
+    .withRule(
+        detectBot({
+            mode: 'LIVE',
+            allow: [],
+        }),
+    )
+    .withRule(
+        fixedWindow({
+            mode: 'LIVE',
+            window: '1m',
+            max: 5,
+        }),
+    );
 
 export async function editCourse(data: CourseSchemaType, courseId: string): Promise<ApiResponse> {
     const user = await requireAdmin();
 
     try {
+        const req = await request();
+        const decision = await aj.protect(req, {
+            fingerprint: user.user.id,
+        });
+
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit()) {
+                return {
+                    status: 'error',
+                    message: 'You have been blocked due to rate limiting',
+                };
+            } else {
+                return {
+                    status: 'error',
+                    message: 'You are a bot! if this a mistake contact our support',
+                };
+            }
+        }
+
         const result = courseSchema.safeParse(data);
 
         if (!result.success) {
