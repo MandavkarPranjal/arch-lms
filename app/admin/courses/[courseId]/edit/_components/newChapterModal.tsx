c57c4768ee5509aa7d3fef5ec6c13bf83b71b47a
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-07-31 20:52:02 +0530

    feat: modal reset

--- app/admin/courses/[courseId]/edit/_components/newChapterModal.tsx
+++ app/admin/courses/[courseId]/edit/_components/newChapterModal.tsx
@@ -58,6 +58,10 @@ export function NewChapterModal({ courseId }: { courseId: string }) {
     }
 
     function handleOpenChange(open: boolean) {
+        if (!open) {
+            form.reset();
+        }
+
         setIsOpen(open);
     }
     return (
