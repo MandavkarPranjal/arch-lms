eb8924d62e3af580cf6b5fa0c31ba6b7ca1b43d2
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-08-01 14:21:53 +0530

    feat: delete course

---
+++ app/admin/courses/[courseId]/delete/page.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { useParams, useRouter } from 'next/navigation';
+import { Loader2, Trash2 } from 'lucide-react';
+import { tryCatch } from '@/hooks/try-catch';
+import { deleteCourse } from './actions';
+import { useTransition } from 'react';
+import { toast } from 'sonner';
+import Link from 'next/link';
+
+export default function DeleteCourseRoute() {
+    const [pending, startTransition] = useTransition();
+    const { courseId } = useParams<{ courseId: string }>();
+    const router = useRouter();
+
+    function onSubmit() {
+        startTransition(async () => {
+            const { data: result, error } = await tryCatch(deleteCourse(courseId));
+
+            if (error) {
+                toast.error('An unexpected error occurred. Please try again later.');
+            }
+
+            if (result?.status === 'success') {
+                toast.success(result.message);
+                router.push('/admin/courses');
+            } else if (result?.status === 'error') {
+                toast.error(result.message);
+            }
+        });
+    }
+
+    return (
+        <div className="mx-auto w-full max-w-xl">
+            <Card className="mt-32">
+                <CardHeader>
+                    <CardTitle>Are you sure you want to delete this course?</CardTitle>
+                    <CardDescription>This action cannot be undone.</CardDescription>
+                </CardHeader>
+                <CardContent className="flex items-center justify-between">
+                    <Link href="/admin/courses" className={buttonVariants({ variant: 'outline' })}>
+                        Cancel
+                    </Link>
+
+                    <Button variant="destructive" onClick={onSubmit} disabled={pending}>
+                        {pending ? (
+                            <>
+                                <Loader2 className="size-4 animate-spin" />
+                                Deleting...
+                            </>
+                        ) : (
+                            <>
+                                <Trash2 className="size-4" />
+                                Delete Course
+                            </>
+                        )}
+                    </Button>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
